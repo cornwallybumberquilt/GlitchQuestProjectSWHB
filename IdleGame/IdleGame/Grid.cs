9109c0a188282b31f847d4aff5616c7004751757
LizardWindow <147208679+LizardWindow@users.noreply.github.com>
2024-04-02 12:21:55 -0400

    Add files via upload
    Made blip visible by changing how grid is displayed

---
+++ IdleGame/IdleGame/Grid.cs
@@ -0,0 +1,90 @@
+﻿namespace IdleGame
+{
+    /// <summary>
+    /// Class for creating a grid object
+    /// </summary>
+    internal class Grid : Panel
+    {
+        public int cellSize { get; set; }
+        public int numRows { get; set; }
+        public int numCols { get; set; }
+        bool renderLines { get; set; }
+        public List<(int, int)> cells;
+        public List<TileOccupier> tileOccupiers;
+
+        /// <summary>
+        /// Parameterized constructor
+        /// </summary>
+        /// <param name="cellSize"></param>
+        /// <param name="numRows"></param>
+        /// <param name="numCols"></param>
+        /// <param name="renderLines"></param>
+        public Grid(int cellSize, int numRows, int numCols, bool renderLines = false)
+        {
+            this.cellSize = cellSize;
+            this.numRows = numRows;
+            this.numCols = numCols;
+            this.Height = this.numRows * this.cellSize;
+            this.Width = this.numCols * this.cellSize;
+            this.renderLines = renderLines;
+            this.BackColor = Color.Black;
+            this.cells = new List<(int, int)>();
+            this.tileOccupiers = new List<TileOccupier>();
+            for (int x = 0; x < numRows; x++)
+            {
+                for (int y = 0; y < numCols; y++)
+                {
+                    //draw tileOccupiers using this? this gets how big something should be and where it should be, so yeah
+                    this.cells.Add((x * cellSize, y * cellSize));
+
+                    this.tileOccupiers.Add(new TileOccupier(cellSize, x, y));
+                    this.tileOccupiers[^1].Draw(x * cellSize, y * cellSize);
+                }
+            }
+            int u = 1;
+        }
+        /// <summary>
+        /// Left blank in starter code, likely used to update grid size
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Update(int dt)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// This will be used to draw the grid to the screen in winforms
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Draw(object sender, PaintEventArgs data)
+        {
+            Panel panel = sender as Panel;
+            Graphics graphics = data.Graphics;
+
+            // this sets the grid line color
+            Pen pen = new Pen(Color.Yellow);
+
+            // Draw horizontal lines
+            for (int i = 0; i <= numRows; i++)
+            {
+                int y = i * cellSize;
+                graphics.DrawLine(pen, 0, y, Width, y);
+            }
+
+            // Draw vertical lines
+            for (int i = 0; i <= numCols; i++)
+            {
+                int x = i * cellSize;
+                graphics.DrawLine(pen, x, 0, x, Height);
+            }
+
+            pen.Dispose();
+        }
+        /// <summary>
+        /// This is a switch to toggle lines of the grid on and off
+        /// </summary>
+        public void ToggleLines()
+        {
+            this.renderLines = !this.renderLines;
+        }
+    }
+}
---
+++ IdleGame/IdleGame/TileOccupier.cs
@@ -0,0 +1,34 @@
+﻿namespace IdleGame
+{
+    internal class TileOccupier : Panel
+    {
+        public int _size;
+        public int _x, _y;
+        public Color _color;
+
+        public TileOccupier(int size, int x, int y)
+        {
+            this._size = size;
+            this._x = x;
+            this._y = y;
+            this.BackColor = RandomColor();
+        }
+        public void Draw(int x, int y)
+        {
+            //This can be used to draw the panel to the screen once you make it visible
+            this.Location = new Point(x, y);
+        }
+        public Color RandomColor()
+        {
+            Random r = new Random();
+
+            r.Next(0, 2);
+            if (r.Next(0, 5) == 0)
+            {
+                return Color.Blue;
+            }
+
+            return Color.Green;
+        }
+    }
+}
